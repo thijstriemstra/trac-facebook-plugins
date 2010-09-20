c10d2ea37deb79231ddab2db97fbf11006c5811e
Thijs Triemstra <info@collab.nl>
2010-09-20 23:32:29 +0200

    add setup file.

---
+++ setup.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2010 The TracFacebookPlugins Project.
+# See LICENSE.txt for details.
+
+from setuptools import find_packages, setup
+
+setup(
+    name='TracFacebookPlugins',
+    author='Thijs Triemstra',
+    author_email='info@collab.nl',
+    description='Support for Facebook plugins in Trac.',
+    url='http://github.com/collab-project/trac-facebook-plugins',
+    license='MIT',
+    version='0.1a1',
+    packages=find_packages(exclude=['*.tests*']),
+    entry_points = """
+        [trac.plugins]
+        facebookplugins = facebookplugins
+    """,
+)
